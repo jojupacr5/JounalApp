19c753eea9d3f0033f5eee3f67d9acddbe14d6cb
jojupacr5 <joseph.paucar@gmail.com>
2024-08-13 14:33:28 -0500

    Inicializando el proyecto, usando react router para las rutas y estructurando las carpetas del proyecto

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+import { JournalApp } from './JournalApp.jsx'
+
+import './styles.css'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <JournalApp />
+    </BrowserRouter>
+  </StrictMode>,
+)
