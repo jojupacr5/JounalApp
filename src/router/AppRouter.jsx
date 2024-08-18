1351150740fdf0fcea1dac77a2bb371d8df78243
Joseph Paucar <mailme@josephpaucar.dev>
2024-08-18 13:46:15 -0500

    Se agrego un customhook con la funcionalidad de hacer login o logout si el usuario esta logeado en firebase

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,23 +1,34 @@
-import { useSelector } from "react-redux"
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
-import { JournalRoutes } from "../journal/routes/JournalRoutes"
 import { CheckingAuth } from "../ui"
+
+import { JournalRoutes } from "../journal/routes/JournalRoutes"
+import { useCheckAuth } from "../hooks"
+
+
 
 export const AppRouter = () => {
 
-  const { status } = useSelector( state => state.auth );
-  
+  const {status} = useCheckAuth();
+
   if( status === 'checking') return <CheckingAuth />
 
   return (
     <Routes>
 
+      {
+        status === 'authenticated'
+        ? <Route path="/*" element={ <JournalRoutes /> } />
+        : <Route path="/auth/*" element={ <AuthRoutes /> } />
+      }
+
+      <Route path="/*" element={ <Navigate to='/auth/login' />} />
+
       {/* Login y Registro */}
-      <Route path="/auth/*" element={ <AuthRoutes /> } />
+      {/* <Route path="/auth/*" element={ <AuthRoutes /> } /> */}
 
       {/* JournalApp */}
-      <Route path="/*" element={ <JournalRoutes /> } />
+      {/* <Route path="/*" element={ <JournalRoutes /> } /> */}
       
     </Routes>
   )
