d956ae65114aa03fd57ffc10e16583f286ec3061
jojupacr5 <joseph.paucar@gmail.com>
2024-08-18 23:27:15 -0500

    Se realizo la funcionalidad de cargar todas las notas y agregarlas al estado, se actualizo el SideBar para desplegar todas las notas y se creo SideBarItem para mejorar la legibilidad

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,12 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, savingNewNote, setActiveNote } from "./journalSlice";
+import {
+  addNewEmptyNote,
+  savingNewNote,
+  setActiveNote,
+  setNotes,
+} from "./journalSlice";
+import { loadNotes } from "../../helpers/loadNotes";
 
 export const startNewNote = () => {
   return async (dispatch, getState) => {
@@ -22,5 +28,16 @@ export const startNewNote = () => {
     // TODO: dispatch
     dispatch(addNewEmptyNote(newNote));
     dispatch(setActiveNote(newNote));
+  };
+};
+
+export const startLoadingNotes = () => {
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth;
+    if (!uid) throw new Error("El UID del usuario no existe");
+
+    const notes = await loadNotes(uid);
+
+    dispatch(setNotes(notes));
   };
 };
