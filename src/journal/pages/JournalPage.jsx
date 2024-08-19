76d7e4681f60eac5466312c26bc119a9f1498f59
jojupacr5 <joseph.paucar@gmail.com>
2024-08-18 22:53:06 -0500

    Actualizando los actions de nuestro slice,  addNewEmptyNote y setActiveNote. se creo tambien el action savingNewNote para deshabilitar el boton, ahora el boton agrega una nueva nota a firestore

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,18 +1,34 @@
+import { useDispatch, useSelector } from "react-redux"
+
+import { startNewNote } from "../../store/journal/thunks"
 import { IconButton } from "@mui/material"
-import { JournalLayout } from "../layout/JournalLayout"
-import { NoteView, NothingSelectedView } from "../views"
 import { AddOutlined } from "@mui/icons-material"
 
+import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
+
+
 export const JournalPage = () => {
+
+  const {isSaving, active} = useSelector(state => state.journal)
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch( startNewNote() )
+  }
+
   return (
     <JournalLayout className="animate__animated animate__fadeIn animate__faster">
-      {/* <Typography variant='h1'>Journal Page</Typography> */}
-
-      {/* <NothingSelectedView /> */}
-      
-      <NoteView />
+   
+      { 
+        (active) 
+        ? <NoteView />
+        : <NothingSelectedView />
+      }
 
       <IconButton
+        disabled={isSaving}
+        onClick={onClickNewNote}
         size="large"
         sx={{ color: 'white', backgroundColor: 'error.main', ':hover': { backgroundColor: 'error.main', opacity: 0.9 }, position: 'fixed', right: 50, bottom: 50 }}
       >
---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const journalSlice = createSlice({
+  name: "journal",
+  initialState: {
+    isSaving: false,
+    messageSaved: "",
+    notes: [],
+    active: null,
+    // active: {
+    //   id: 'abc123',
+    //   title: '',
+    //   body: '',
+    //   date: 12312435,
+    //   imageUrls: [], // lista de urls de imagenes
+    // }
+  },
+  reducers: {
+    savingNewNote: (state) => {
+      state.isSaving = true;
+    },
+    addNewEmptyNote: (state, action) => {
+      state.notes.push(action.payload);
+      state.isSaving = false;
+    },
+    setActiveNote: (state, action) => {
+      state.active = action.payload;
+    },
+    setNotes: (state, action) => {},
+    setSaving: (state) => {},
+    updateNote: (state, action) => {},
+    deleteNoteyyId: (state, action) => {},
+  },
+});
+
+export const {
+  addNewEmptyNote,
+  setActiveNote,
+  setNotes,
+  setSaving,
+  updateNote,
+  deleteNoteyyId,
+  savingNewNote,
+} = journalSlice.actions;
